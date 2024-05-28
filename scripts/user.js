e48b64fa04d7b29fc0f581932860c12b2e6e62c0
Sasha <alex.rud.seal@gmail.com>
2024-05-28 13:17:59 +0700

    initial commit

---
+++ scripts/user.js
@@ -0,0 +1,8 @@
+function showBook(){
+    document.getElementById('book-tab').style.display = 'block';
+    document.getElementById('form-tab').style.display = 'none';
+}
+function showForm(){
+    document.getElementById('book-tab').style.display = 'none';
+    document.getElementById('form-tab').style.display = 'block';
+}
